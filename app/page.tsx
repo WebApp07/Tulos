dadd201adae866b5f1df1b411bb3ff794c92a3b1
WebApp07 <aminezouguig@proton.me>
2026-07-20 15:29:43 +0200

    project-setup initialize Next.js project with TypeScript, Tailwind CSS, and shadcn/ui

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Tulos Ecommerce Website</h2>
+      <Button variant="outline">Add To Cart</Button>
+    </div>
+  );
+}
