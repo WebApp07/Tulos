d5b5b638f9614d49840e2ccb3f7e982d029fffde
WebApp07 <aminezouguig@proton.me>
2026-07-21 16:01:37 +0200

    Setup Sanity cms, Build Ui Sidebar responsive Ui

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import React, { Children } from "react";
 interface Props {
   children: React.ReactNode;
   className?: "string";
---
+++ components/SocialMedia.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import Link from "next/link";
+
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+
+import { cn } from "@/lib/utils";
+interface Props {
+  className?: string;
+  iconClassName?: string;
+  tooltipClassName?: string;
+}
+
+const socialLink = [
+  {
+    title: "Facebook",
+    href: "https://facebook.com/yourusername",
+    icon: <FaFacebook className="w-5 h-5" />,
+  },
+  {
+    title: "Instagram",
+    href: "https://instagram.com/yourusername",
+    icon: <FaInstagram className="w-5 h-5" />,
+  },
+];
+const SocialMedia = ({ className, iconClassName, tooltipClassName }: Props) => {
+  return (
+    <TooltipProvider>
+      <div className={cn("flex items-center gap-3.5", className)}>
+        {socialLink?.map((item) => (
+          <Tooltip key={item?.title}>
+            <TooltipTrigger asChild>
+              <Link
+                href={item?.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={cn(
+                  "p-2 border rounded-full hover:text-white hover:border-white hoverEffect",
+                  iconClassName,
+                )}
+              >
+                {item?.icon}
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent
+              className={cn(
+                "bg-white text-darkColor font-semibold",
+                tooltipClassName,
+              )}
+            >
+              {item?.title}
+            </TooltipContent>
+          </Tooltip>
+        ))}
+      </div>
+    </TooltipProvider>
+  );
+};
+
+export default SocialMedia;
