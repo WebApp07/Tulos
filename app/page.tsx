d8c3b3f9a7eb1b7ce823aea13ef3a339343d86c7
WebApp07 <aminezouguig@proton.me>
2026-07-21 12:34:01 +0200

    Navbar ui design

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h2>Tulos Ecommerce Website</h2>
-      <Button variant="outline">Add To Cart</Button>
+      <Container>
+        <h2>Tulos Ecommerce Website</h2>
+        <Button variant="outline">Add To Cart</Button>
+      </Container>
     </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import CartIcon from "./CartIcon";
+import Container from "./Container";
+import HeaderMenu from "./HeaderMenu";
+import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
+import SearchBar from "./SearchBar";
+
+const Header = () => {
+  return (
+    <header className="border-b border-b-gray-400 py-5">
+      <Container className="flex items-center justify-between gap-7 text-lightColor">
+        <HeaderMenu />
+        <div className="w-auto md:w-1/3 flex items-center justify-center gap-2.5">
+          <MobileMenu />
+          <Logo>Tulos</Logo>
+        </div>
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+        </div>
+        <div>
+          <button className="text-sm font-semibold hover:text-darkColor hoverEffect">
+            Login
+          </button>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
