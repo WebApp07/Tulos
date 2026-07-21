523506f5555e549a5ba7a3dc338964d5d9ef2601
WebApp07 <aminezouguig@proton.me>
2026-07-21 16:48:18 +0200

    Setup sanity as backend cms, generate types category, product, connected our app with sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { categoryType } from "./categoryType";
+import { productType } from "./productType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [categoryType, productType],
+};
