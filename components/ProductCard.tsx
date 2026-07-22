4d1798dad2aaf442d91cb60f5a6ede6fdf543c8c
WebApp07 <aminezouguig@proton.me>
2026-07-22 15:31:32 +0200

    Build component Add to cart

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import PriceView from "./PriceView";
+import AddToCartButton from "./AddToCartButton";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
@@ -37,6 +38,7 @@ const ProductCard = ({ product }: { product: Product }) => {
           price={product?.price}
           discount={product?.discount}
         />
+        <AddToCartButton product={product} />
       </div>
     </div>
   );
