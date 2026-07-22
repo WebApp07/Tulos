d310dcd60ad1a78264247819ed3684881f0b7df6
WebApp07 <aminezouguig@proton.me>
2026-07-22 02:24:19 +0200

    we call our products from cms sanity, We build components to show products in frontend

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,13 +1,13 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import ProductGrid from "@/components/ProductGrid";
 
 export default function Home() {
   return (
     <div className="">
       <Container className="py-10">
         <HomeBanner />
+        <ProductGrid />
       </Container>
     </div>
   );
